bb48eabfa735884d20cc1af4c403466fa8378520
Maxim <maxim.yumakov@gmail.com>
2022-03-18 11:28:01 +0400

    Creat portfolio

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,10 @@ const Header = (
       <div className="menu"></div>
       <nav className="menu">
          <div className="menu-item"><a href="#" className="link-item-menu">New Perceprion</a></div>
-         <div className="menu-item"><a href="Port/Port.html" className="link-item-menu">Портфолио</a></div>
-         <div className="menu-item"><a href="News/news.html" className="link-item-menu">Новости</a></div>
-         <div className="menu-item"><a href="About/About.html" className="link-item-menu">О компании</a></div>
-         <div className="menu-item"><a href="Order/Order.html" className="link-item-menu">Заказать</a></div>
+         <div className="menu-item"><a href="Port/port.jsx" className="link-item-menu">Портфолио</a></div>
+         <div className="menu-item"><a href="News/news.jsx" className="link-item-menu">Новости</a></div>
+         <div className="menu-item"><a href="About/about.jsx" className="link-item-menu">О компании</a></div>
+         <div className="menu-item"><a href="Order/order.jsx" className="link-item-menu">Заказать</a></div>
       </nav>
    </header>
 )
