342f0289efb06ea99d626c634a7e512da4585e0a
Maxim <maxim.yumakov@gmail.com>
2022-03-19 12:13:38 +0400

    add fucntion

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 
-const Header = (
-   <header>
-      <div className="menu"></div>
-      <nav className="menu">
-         <div className="menu-item"><a href="#" className="link-item-menu">New Perceprion</a></div>
-         <div className="menu-item"><a href="Port/port.jsx" className="link-item-menu">Портфолио</a></div>
-         <div className="menu-item"><a href="News/news.jsx" className="link-item-menu">Новости</a></div>
-         <div className="menu-item"><a href="About/about.jsx" className="link-item-menu">О компании</a></div>
-         <div className="menu-item"><a href="Order/order.jsx" className="link-item-menu">Заказать</a></div>
-      </nav>
-   </header>
-)
+const Header = () => {
+   return (
+      <header>
+         <div className="menu"></div>
+         <nav className="menu">
+            <div className="menu-item"><a href="#" className="link-item-menu">New Perceprion</a></div>
+            <div className="menu-item"><a href="Port/port.jsx" className="link-item-menu">Портфолио</a></div>
+            <div className="menu-item"><a href="News/news.jsx" className="link-item-menu">Новости</a></div>
+            <div className="menu-item"><a href="About/about.jsx" className="link-item-menu">О компании</a></div>
+            <div className="menu-item"><a href="Order/order.jsx" className="link-item-menu">Заказать</a></div>
+         </nav>
+      </header>
+   )
+}
 
 const root = document.getElementById("root");
-ReactDOM.render(Header, root);
+ReactDOM.render(<Header />, root);
