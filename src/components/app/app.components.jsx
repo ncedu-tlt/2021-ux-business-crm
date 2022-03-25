9f9de34ae7af909fe8d00a55c78c40db2d0d3ff7
Maxim <maxim.yumakov@gmail.com>
2022-03-25 09:08:25 +0400

    Add imports

--- src/components/app/app.components.jsx
+++ src/components/app/app.components.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom'
-import Header from '../Header/Header.component'
+import Header from "../Header/Header.components";
 
 const App = () => {
    return (
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 import './styles/index.css';
 /*import Header from './components/Header/Header.components';*/
-import App from './components/app/app.component.jsx'
+/*import App from './components/app/app.component.jsx'*/
+import App from './components/app/app.components';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 
