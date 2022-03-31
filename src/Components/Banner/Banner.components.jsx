2f1b09b9d5e75ca942b4ec1e990b19ea3890ff72
Loyr123321 <92223466+Loyr123321@users.noreply.github.com>
2022-03-31 17:34:01 +0300

    Доработка
    Учтены замечания. Переименован файл index css, убраны лишние комментарии

--- src/Components/Banner/Banner.components.jsx
+++ src/Components/Banner/Banner.components.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import './index.css';
+import './Banner.components.css';
 
 
 const Banner = () => {
@@ -23,10 +23,5 @@ const Banner = () => {
     </div >
   )
 }
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 export default Banner;
