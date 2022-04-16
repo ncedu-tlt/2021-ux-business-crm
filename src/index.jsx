0355d82b9f6c77fdf901efc279bd6bfe3bbe7498
Loyr123321 <92223466+Loyr123321@users.noreply.github.com>
2022-04-17 01:32:03 +0300

    Delete index.jsx

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import Feedback from './components/feedback/feedback.component';
-import {render} from 'react-dom'
-
-
-render(<Feedback />, document.getElementById("root"))
