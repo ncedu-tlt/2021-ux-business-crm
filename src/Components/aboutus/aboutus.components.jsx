46a0d6f6680c5cc71f3c9f1365b427194dc80036
Maxim <maxim.yumakov@gmail.com>
2022-05-12 14:38:48 +0400

    create ner components

---
+++ src/Components/aboutus/aboutus.components.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './aboutus.components.css';
+import { ReactComponent as Us } from '../../Img/us.jpg';
+
+const aboutus = () => {
+   return (
+      <div className="wrapper">
+         <div className="text">Наша компания занимается разработкой VR/AR приложений. С 2021 года мы являемся лидером в сфере AR</div>
+         <div className="image">
+            <Us className='aboutus__img' />
+         </div>
+      </div>
+   )
+}
+
+export default aboutus
