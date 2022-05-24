873a60f9a6c8f1a4ca30695886b9ff2634ed13ba
Maxim <maxim.yumakov@gmail.com>
2022-05-24 23:40:35 +0400

    2 coloumns

--- src/сomponents/about-us/aboutus.component.jsx
+++ src/сomponents/about-us/aboutus.component.jsx
@@ -4,12 +4,24 @@ import Personal from '../../img/us.jpg';
 
 const AboutUs = () => {
    return (
+      // <div className="wrapper">
+      //    <div className="title">Про нашу команду</div>
+      //    <div className="text">Наша компания занимается разработкой VR/AR приложений.</div>
+      //    <div className="text">С 2021 года мы являемся лидером в сфере AR</div>
+      //    <div className="image">
+      //       <img src={Personal} className='aboutus__img' alt="it's me" />
+      //    </div>
+      // </div>
       <div className="wrapper">
          <div className="title">Про нашу команду</div>
-         <div className="text">Наша компания занимается разработкой VR/AR приложений.</div>
-         <div className="text">С 2021 года мы являемся лидером в сфере AR</div>
-         <div className="image">
-            <img src={Personal} className='aboutus__img' alt="it's me" />
+         <div className="textcols-item">
+            Наша компания занимается разработкой VR/AR приложений. С 2021 года мы являемся лидером в сфере AR
+         </div>
+         <div className="textcols-item">
+            Наши работники
+            <div className="image">
+               <img src={Personal} className='aboutus__img' alt="it's me" />
+            </div>
          </div>
       </div>
    )
