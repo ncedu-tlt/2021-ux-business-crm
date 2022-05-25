ce644867c6bd61025c13e54844371b0584ea1661
Maxim <maxim.yumakov@gmail.com>
2022-05-25 12:57:40 +0400

    change Route

--- src/сomponents/app/app.component.jsx
+++ src/сomponents/app/app.component.jsx
@@ -5,18 +5,18 @@ import MainPage from "../../pages/main/main.components.jsx"
 import AboutCompany from "../../pages/about-company/about-company.component.jsx";
 import NewsPage from "../../pages/news/news.components";
 import Portfolio from "../../pages/portfolio/portfolio.component";
-import Order from "../../pages/order/order.component.jsx";
+import OrderPage from "../../pages/order/order.component.jsx";
 
 const App = () => {
     return (
         <div>
             <Header />
             <Routes>
-                <Route path="/" element={<MainPage />}></Route>
-                <Route path="portfolio.components.jsx" element={<Portfolio />}></Route>
-                <Route path="news.components" element={<NewsPage />}></Route>
-                <Route path="about-company.component" element={<AboutCompany />}></Route>
-                <Route path="order.component" element={<Order />}></Route>
+                <Route path="/" exact element={<MainPage />}></Route>
+                <Route path="/portfolio" element={<Portfolio />}></Route>
+                <Route path="/news" element={<NewsPage />}></Route>
+                <Route path="/about-us" element={<AboutCompany />}></Route>
+                <Route path="/order" element={<OrderPage />}></Route>
             </Routes>
         </div>
     )
