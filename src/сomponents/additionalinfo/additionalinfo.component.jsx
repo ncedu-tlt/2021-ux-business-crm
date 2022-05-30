fd5c655b698f37a9634f1588d42d73db011c54c2
naimlukianov <salomat56@gmail.com>
2022-05-31 01:10:40 +0400

    additionalinfo

---
+++ src/сomponents/additionalinfo/additionalinfo.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './additionalinfo.component.css';
+import ReactPlayer from 'react-player';
+
+const AdditionalInfo = () => {
+return (
+<div className='wrapper'>
+<div className='info'>
+<h1>О дополненной реальности пару слов</h1>
+</div>
+<div className='video'>
+<ReactPlayer url="https://youtu.be/cNStlZljJIc" controls={true} />
+</div>
+<div className='description'>
+<div className='description__1'>Примерчик и его описание пару слов</div>
+<div className='description__2'>Примерчик и его описание пару слов</div>
+<div className='description__3'>Примерчик и его описание пару слов</div>
+<div className='description__4'>Примерчик и его описание пару слов</div>
+</div>
+</div>
+)
+}
+
+export default AdditionalInfo
