e39efc209787953ced18831014d757cb941c6061
Maxim <maxim.yumakov@gmail.com>
2022-05-30 23:26:09 +0400

    Update header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,11 +6,11 @@ const Header = () => {
    return (
       <header>
          <nav className="menu">
-            <div className="menu-item"><Link to="/AdditionalInfo" className="link-item-menu">New Perceprion</Link></div>
-            <div className="menu-item"><Link to="/" className="link-item-menu">Портфолио</Link></div>
-            <div className="menu-item"><Link to="/" className="link-item-menu">Новости</Link></div>
-            <div className="menu-item"><Link to="/" className="link-item-menu">О компании</Link></div>
-            <div className="menu-item"><Link to="/" className="link-item-menu">Заказать</Link></div>
+            <div className="menu-item"><Link to="/" className="link-item-menu">New Perceprion</Link></div>
+            <div className="menu-item"><Link to="/portfolio" className="link-item-menu">Портфолио</Link></div>
+            <div className="menu-item"><Link to="/news" className="link-item-menu">Новости</Link></div>
+            <div className="menu-item"><Link to="/about-us" className="link-item-menu">О компании</Link></div>
+            <div className="menu-item"><Link to="/order" className="link-item-menu">Заказать</Link></div>
          </nav>
       </header>
    )
