a7ca21d20cc45305f9ba06667ff2f508b8dd90a3
relayt <needcoffeepills@gmail.com>
2022-06-23 18:47:59 +0400

    banner

--- src/pages/news/news.components.jsx
+++ src/pages/news/news.components.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './news.components.css';
+import Newsbanner from "../../сomponents/news-banner/newsbanner.component";
 
 const NewsPage = () => {
    return (
       <div className="news">
-         <div className="text">Тут можно будут новости</div>
+         <div className="text">Новости</div>
+         <Newsbanner/>
       </div>
    )
 }
