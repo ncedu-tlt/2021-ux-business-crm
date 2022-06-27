f4d09560b922eb25c786c034d07a138912519589
Maxim <maxim.yumakov@gmail.com>
2022-06-27 18:09:02 +0400

    change margin

--- src/сomponents/about-us/aboutus.component.jsx
+++ src/сomponents/about-us/aboutus.component.jsx
@@ -6,14 +6,9 @@ const AboutUs = () => {
    return (
       <div className="wrapper">
          <div className="title">Про нашу команду</div>
-         <div className="textcols-item">
+         <img src={Personal} className='aboutus__img' alt="it's me" />
+         <div className="text">
             Наша компания занимается разработкой VR/AR приложений. С 2021 года мы являемся лидером в сфере AR
-         </div>
-         <div className="textcols-item">
-            Наши работники
-            <div className="image">
-               <img src={Personal} className='aboutus__img' alt="it's me" />
-            </div>
          </div>
       </div>
    )
